a39a4f837739caafa15b062c8d94fb17a9557964
Chinmmayie <chinnmayie.nagam@gmail.com>
2026-08-19 17:49:07 +0530

    changed the groq model

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def parse_resume_with_groq(resume_text: str) -> dict:
     """
     
     response = client.chat.completions.create(
-        model="llama-3.3-70b-versatile",
+        model="llama-3.1-8b-instant",
         response_format={"type": "json_object"},
         messages=[{"role": "user", "content": prompt}],
         temperature=0.1,
@@ -84,7 +84,7 @@ def evaluate_job(candidate_profile: dict, job: dict) -> dict:
     """
     
     response = client.chat.completions.create(
-        model="llama-3.3-70b-versatile",
+        model="llama-3.1-8b-instant",
         response_format={"type": "json_object"},
         messages=[{"role": "user", "content": prompt}],
         temperature=0.1,
